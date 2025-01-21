361d34bbec2dbb3f5dbb3290ea1611911cbf12af
Dennis E <git@destenson.digital>
2025-01-20 20:49:43 -0800

    Revert "continue windows implementation (I broke something)"
    This reverts commit afd79ae5f7867adf2b1ade3c4d797d820d75c8e0.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ mod platform;
 use platform::main as pmain;
 
 
-fn main() -> Result<(), Box<impl std::error::Error>> {
+fn main() {
     // if windows target, run win32_main::main
 
     std::env::set_var("RUST_LOG", "trace");
 
     env_logger::init();
 
-    pmain() //.unwrap();//.expect("win32_main::main failed");
-        .map_err(|e| {
-            eprintln!("Error: {}", e);
-            Box::new(e)
-        })
+    pmain().unwrap();//.expect("win32_main::main failed");
 }
